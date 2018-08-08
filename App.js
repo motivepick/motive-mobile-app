b37688ab0e777d84e12cd3abf22f56191fd222cc
yaskovdev <yaskovdev@gmail.com>
2018-08-08 21:54:46 +0300

    Create repository for mobile app with existing functionality #3

--- App.js
+++ App.js
@@ -1,49 +1,36 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {LoginButton} from 'react-native-fbsdk';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <LoginButton
+                    readPermissions={["email"]}
+                    onLoginFinished={
+                        (error, result) => {
+                            if (error) {
+                                this.setState({error: error});
+                                alert("Login failed with error: " + error.message);
+                            } else if (result.isCancelled) {
+                                alert("Login was cancelled");
+                            } else {
+                                alert("Login was successful with permissions: " + result.grantedPermissions)
+                            }
+                        }
+                    }
+                    onLogoutFinished={() => alert("User logged out")}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('MotiveMobileApp', () => App);
