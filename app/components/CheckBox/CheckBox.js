e7e477851a8cafb7aa6ed954f7f5fec68b68e066
yaskovdev <yaskovdev@gmail.com>
2018-08-23 19:58:55 +0300

    [Mobile] Stable UI for IOS app #10
    - Cleanup code.

--- app/components/CheckBox/CheckBox.js
+++ app/components/CheckBox/CheckBox.js
@@ -1,26 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { TouchableOpacity, View } from 'react-native'
 import styles from './CheckBox.styles'
 
-export class CheckBox extends Component {
-    constructor(props) {
-        super(props)
-    }
+const CheckBox = (props) => {
+    const { isCompleted, onAction } = props
 
-    render() {
-        const { isCompleted, onAction } = this.props
-
-        return (
-            <TouchableOpacity onPress={onAction}>
-                <View
-                    style={[
-                        styles.circle,
-                        isCompleted ? styles.completeCircle : styles.incompleteCircle
-                    ]}
-                />
-            </TouchableOpacity>
-        )
-    }
+    return (
+        <TouchableOpacity onPress={onAction}>
+            <View style={[styles.circle, isCompleted ? styles.completeCircle : styles.incompleteCircle]}/>
+        </TouchableOpacity>
+    )
 }
 
 export default CheckBox
