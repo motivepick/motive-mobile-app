a3b73eb79ef035aa2368e03dae64e50d9da375f9
Jelena <jelena.shaipuka@gmail.com>
2018-08-26 12:20:05 +0300

    Palette to indicator

--- app/components/ColorIndicator/ColorIndicator.js
+++ app/components/ColorIndicator/ColorIndicator.js
@@ -2,9 +2,17 @@ import React from 'react'
 import { View } from 'react-native'
 import styles from './ColorIndicator.styles'
 
+const palette = {
+    red: '#C25B56',
+    orange: '#FFAE5D',
+    blue: '#96C0CE',
+    deepBlue: '#336699',
+    purple: '#6F3662'
+}
+
 export const ColorIndicator = (props) => {
     const { color, styler } = props
-    return <View style={[styles.colorTagCircle, color ? { backgroundColor: color } : {}, styler]}/>
+    return <View style={[styles.colorTagCircle, color ? { backgroundColor: palette[color] } : {}, styler]}/>
 }
 
 export default ColorIndicator
