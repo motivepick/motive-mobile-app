51d6ca2d7d74d6ef15ee80c55e20206785c562a2
Jelena <jelena.shaipuka@gmail.com>
2018-09-06 19:29:41 +0300

    Header to goal screen

--- app/screens/GoalScreen.js
+++ app/screens/GoalScreen.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react'
-import { Container } from 'native-base'
+import { Container, Content, H1 } from 'native-base'
 import TaskList from '../components/TaskList/TaskList'
 import { translate } from 'react-i18next'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { setGoalAction } from '../actions/goalsActions'
+import Header from '../components/common/Header/Header'
 
 class GoalScreen extends Component {
+
+    static navigationOptions = {
+        header: null
+    }
 
     componentDidMount() {
         const { navigation, setGoal } = this.props
         const goal = navigation.getParam('goal')
-        console.log('going to set goal', goal)
         setGoal(goal)
     }
 
     render() {
-        const { goal, createGoalTask, updateGoalTasks, deleteTask } = this.props
+        const { goal, navigation, createGoalTask, updateGoalTasks, deleteTask } = this.props
         const { id, tasks } = goal
         return (
             <Container>
-                <TaskList tasks={tasks} onTaskCreated={task => createGoalTask(id, task)} onFilterChanged={filter => updateGoalTasks(id, filter)}
-                    onDeleteTask={id => deleteTask(id)}/>
+                <Header title={'Goal'} onLeftButtonPress={() => navigation.goBack()} onRightButtonPress={this.handleGoalClick}/>
+                <Content>
+                    <H1 style={{ textAlign: 'center', paddingTop: 7 }}>{goal.name}</H1>
+                    <TaskList tasks={tasks} onTaskCreated={task => createGoalTask(id, task)} onFilterChanged={filter => updateGoalTasks(id, filter)} onDeleteTask={id => deleteTask(id)}/>
+                </Content>
             </Container>
         )
+    }
+
+    handleGoalClick = () => {
+        const { goal, navigation } = this.props
+        navigation.navigate('GoalEdit', { goal })
     }
 }
 
