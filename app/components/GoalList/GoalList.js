74afd3d6a5ee3aa1f247ddc50282ecc821c5bf2b
Jelena <jelena.shaipuka@gmail.com>
2018-10-21 23:13:19 +0300

    Banrd primary color

--- app/components/GoalList/GoalList.js
+++ app/components/GoalList/GoalList.js
@@ -1,45 +1,25 @@
 import React, { Component } from 'react'
-import { ListView, View } from 'react-native'
 import Goal from '../Goal/Goal'
 import { translate } from 'react-i18next'
 import { Button, Text } from 'native-base'
 import List from '../common/List/List'
 import { withNavigation } from 'react-navigation'
-import { iOSColors } from 'react-native-typography'
 
 export class GoalList extends Component {
 
-    state = { goalName: '' }
-
     constructor(props) {
         super(props)
-        this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
     }
 
     render() {
-        const { goalName } = this.state
-        const { goals, creatingGoal, t } = this.props
+        const { goals } = this.props
 
         return (
-            <View style={{ flex: 1 }}>
-                {/* <Form style={{ marginHorizontal: 10 }}>
-                    <Item regular>
-                        <Input
-                            onChangeText={goalName => this.setState({ goalName })}
-                            value={goalName}
-                            onSubmitEditing={this.onAddNewGoal}
-                            editable={!creatingGoal}
-                            returnKeyType={'done'}
-                            placeholder={t('labels.newGoal')}
-                        />
-                    </Item>
-                </Form> */}
-                <List
-                    data={goals}
-                    renderRow={this.renderRow}
-                    renderRightHiddenRow={this.renderRightHiddenRow}
-                />
-            </View>
+            <List
+                data={goals}
+                renderRow={this.renderRow}
+                renderRightHiddenRow={this.renderRightHiddenRow}
+            />
         )
     }
 
@@ -47,7 +27,7 @@ export class GoalList extends Component {
 
     renderRightHiddenRow = (data, secId, rowId, rowMap) =>
         <Button transparent onPress={() => this.deleteGoal(data, secId, rowId, rowMap)}>
-            <Text style={{ color: iOSColors.pink  }}>{'Delete'.toLocaleUpperCase()}</Text>
+            <Text>{'Delete'.toLocaleUpperCase()}</Text>
         </Button>
 
     deleteGoal = (data, secId, rowId, rowMap) => {
@@ -55,22 +35,6 @@ export class GoalList extends Component {
         const { id } = data
         onDeleteGoal(id)
         rowMap[`${secId}${rowId}`].props.closeRow()
-    }
-
-    editGoal = (data, secId, rowId, rowMap) => {
-        const { navigation } = this.props
-        navigation.navigate('GoalEdit', { goal: data })
-        rowMap[`${secId}${rowId}`].props.closeRow()
-    }
-
-    onAddNewGoal = async () => {
-        const { goalName } = this.state
-        const { onGoalCreated } = this.props
-        if (goalName.trim() !== '') {
-            const goal = { name: goalName.trim() }
-            onGoalCreated(goal)
-            this.setState({ goalName: '' })
-        }
     }
 }
 
--- native-base-theme/variables/platform.js
+++ native-base-theme/variables/platform.js
@@ -106,7 +106,7 @@ export default {
     checkboxTickColor: '#fff',
 
     // Color
-    brandPrimary: platform === 'ios' ? '#007aff' : '#3F51B5',
+    brandPrimary: platform === 'ios' ? iOSColors.pink : '#3F51B5',
     brandInfo: '#62B1F6',
     brandSuccess: '#5cb85c',
     brandDanger: '#d9534f',
