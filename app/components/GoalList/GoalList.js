f7948e436ed8501336b32e4501bf8beb3ccaaf66
Jelena <jelena.shaipuka@gmail.com>
2018-10-31 19:06:21 +0200

    Task style

--- app/components/GoalList/GoalList.js
+++ app/components/GoalList/GoalList.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import Goal from '../Goal/Goal'
 import { translate } from 'react-i18next'
 import { Button, Text } from 'native-base'
 import List from '../common/List/List'
 import { withNavigation } from 'react-navigation'
+import { calculateGoalProgressStats } from '../../utils/progressUtils'
+import CheckboxListItem from '../common/CheckboxListItem/CheckboxListItem'
 
 export class GoalList extends Component {
 
@@ -23,12 +24,40 @@ export class GoalList extends Component {
         )
     }
 
-    renderRow = data => <Goal data={data}/>
+    renderRow = (data, secId, rowId, rowMap) => {
+        const { name, colorTag, dueDate, tasks = [], closed } = data
+        const { percents: { progress }, labels: { taskCountLabel } } = calculateGoalProgressStats(tasks, closed)
+
+        return (
+            <CheckboxListItem
+                isCompleted={closed}
+                onComplete={() => this.onGoalClose(data, secId, rowId, rowMap)}
+                onBodyClick={() => this.handleGoalClick(data)}
+                text={name}
+                noteText={taskCountLabel}
+                date={dueDate}
+                checkboxColor={colorTag}
+                progress = {progress}
+            />
+        )
+    }
 
     renderRightHiddenRow = (data, secId, rowId, rowMap) =>
         <Button transparent onPress={() => this.deleteGoal(data, secId, rowId, rowMap)}>
             <Text>{'Delete'.toLocaleUpperCase()}</Text>
         </Button>
+
+    onGoalClose = (data, secId, rowId, rowMap) => {
+        const { onGoalClose } = this.props
+        const { id } = data
+        onGoalClose(id)
+        rowMap[`${secId}${rowId}`].props.closeRow()
+    }
+
+    handleGoalClick = (data) => {
+        const { navigation } = this.props
+        navigation.navigate('Goal', { goal: data })
+    }
 
     deleteGoal = (data, secId, rowId, rowMap) => {
         const { onDeleteGoal } = this.props
