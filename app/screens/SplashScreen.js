d8a9a61a78070792f050fa4922f024eabc57c1d1
yaskovdev <yaskovdev@gmail.com>
2018-11-15 21:26:02 +0200

    Add another OAuth provider. Facebook OAuth is terrible.

--- app/screens/SplashScreen.js
+++ app/screens/SplashScreen.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import { AsyncStorage, View } from 'react-native'
 import request from 'superagent'
 import { navigateWithReset } from './navigationWithReset'
-import { API_URL } from '../const';
-import CookieManager from 'react-native-cookies';
+import { API_URL } from '../const'
+import CookieManager from 'react-native-cookies'
 
 class SplashScreen extends Component {
 
     async componentDidMount() {
-        let token = await AsyncStorage.getItem('token');
+        const token = await AsyncStorage.getItem('token')
 
         // we have to remove cookies before first request, otherwise it may add multiple SESSION values
         // see https://build.affinity.co/persisting-sessions-with-react-native-4c46af3bfd83 for details
@@ -17,7 +17,7 @@ class SplashScreen extends Component {
                 .set('Cookie', token)
                 .then(() => navigateWithReset(this.props.navigation, 'Home'))
                 .catch(() => navigateWithReset(this.props.navigation, 'Login'))
-        });
+        })
     }
 
     render() {
