95aafb2190c5dd0473d4431a5b2f3821252d1393
Jelena <jelena.shaipuka@gmail.com>
2018-12-11 23:27:00 +0200

    Color Pallette externalized

--- app/components/common/ColorPicker/ColorPicker.js
+++ app/components/common/ColorPicker/ColorPicker.js
@@ -2,24 +2,7 @@ import React, { PureComponent } from 'react'
 import { View } from 'react-native'
 import { Button, Icon } from 'native-base'
 import { translate } from 'react-i18next'
-
-const palette = {
-    maroon: '#800000',
-    red: '#e6194B',
-    pink: '#fabebe',
-    brown: '#9A6324',
-    orange: '#f58231',
-    olive: '#808000',
-    yellow: '#ffe119',
-    lime: '#bfef45',
-    green: '#3cb44b',
-    cyan: '#42d4f4',
-    deepBlue: '#336699',
-    blue: '#4363d8',
-    purple: '#911eb4',
-    lavender: '#e6beff',
-    magenta: '#f032e6'
-}
+import palette from '../Palette'
 
 class ColorPicker extends PureComponent {
 
